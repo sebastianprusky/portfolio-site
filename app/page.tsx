682882b44f488f2018f34f8c82c41a6f76acbb93
sebastianprusky <sebastianprusky2028@u.northwestern.edu>
2025-10-01 22:15:37 -0500

    testing source control

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home(): JSX.Element {
     <main className="p-8 flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-5xl font-bold mb-4">Sebastian Prusky</h1>
       <p className="text-lg text-gray-500 text-center">
-        Welcome to my portfolio!
+        welcome
       </p>
     </main>
   );

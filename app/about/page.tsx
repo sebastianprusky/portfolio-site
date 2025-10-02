b00dfdc4b8893ab30bc627bc19473678811f4a1b
sebastianprusky <sebastianprusky2028@u.northwestern.edu>
2025-10-02 13:45:46 -0500

    about section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ import { JSX } from "react";
 export default function About(): JSX.Element {
   return (
     <main className="p-8 min-h-screen">
-      <h1 className="text-3xl font-bold mb-4">About Me</h1>
+      <h1 className="text-3xl font-bold mb-4">I'm from Miami, FL and currently studying Industrial Engineering, MMSS, and CS at Northwestern University.\nI'm passionate about product design, process optimization, technology, and AI.\n In my free time, I love painting, lifting and running, going to the beach, and watching thriller and horror movies.\n You can reach me at sebastianprusky2028@u.northwestern.edu</h1>
       <p>
         about text
       </p>

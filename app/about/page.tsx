bdacb0081acded8896158e7e3e368105a2db2d26
sebastianprusky <sebastianprusky2028@u.northwestern.edu>
2025-10-02 15:31:27 -0500

    font color and spacing

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,7 @@ export default function About(): JSX.Element {
       <h1 className="text-2xl font-semibold mb-2 text-center">About Me</h1>
       <div className="flex justify-center">
   <p className="text-base font-normal mb-4 text-left max-w-2xl">
-        I&apos;m from Miami, FL and currently studying Industrial Engineering, MMSS, and CS at Northwestern University.<br /><br />
+        <br />I&apos;m from Miami, FL and currently studying Industrial Engineering, MMSS, and CS at Northwestern University.<br /><br />
         I&apos;m passionate about technology, product design, and engineering.<br /><br />
         In my free time, I love painting, lifting and running, spending time at the beach, and watching thriller movies.<br /><br />
         You can reach me at sebastianprusky2028@u.northwestern.edu.
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home(): JSX.Element {
   return (
     <main className="p-8 flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-5xl font-bold mb-4">Sebastian Prusky</h1>
-      <p className="text-lg text-gray-500 text-center">
+      <p className="text-lg text-center">
         IE + MMSS + CS minor @ Northwestern
       </p>
       <div className="flex flex-row gap-6 mt-4">

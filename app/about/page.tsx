893a12894a79c2a00c6e94f824fb09ca643325b9
sebastianprusky <sebastianprusky2028@u.northwestern.edu>
2025-10-02 13:52:08 -0500

    fixed error

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,10 +6,10 @@ export default function About(): JSX.Element {
       <h2 className="text-2xl font-semibold mb-2 text-center">About Me</h2>
       <div className="flex justify-center">
         <h1 className="text-3xl font-bold mb-4 text-left max-w-2xl">
-        I'm from Miami, FL and currently studying Industrial Engineering, MMSS, and CS at Northwestern University.<br />
-        I'm passionate about product design, process optimization, technology, and AI.<br />
+        I&apos;m from Miami, FL and currently studying Industrial Engineering, MMSS, and CS at Northwestern University.<br />
+        I&apos;m passionate about product design, process optimization, technology, and AI.<br />
         In my free time, I love painting, lifting and running, going to the beach, and watching thriller and horror movies.<br />
-        You can reach me at sebastianprusky2028@u.northwestern.edu
+        You can reach me at sebastianprusky2028@u.northwestern.edu.
         </h1>
       </div>
     </main>

b07c2a1c8669627f90c4a7aee3198c4901a3e01a
sebastianprusky <sebastianprusky2028@u.northwestern.edu>
2025-10-02 13:55:11 -0500

    changed text size

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,14 +3,14 @@ import { JSX } from "react";
 export default function About(): JSX.Element {
   return (
     <main className="p-8 min-h-screen">
-      <h2 className="text-2xl font-semibold mb-2 text-center">About Me</h2>
+      <h1 className="text-2xl font-semibold mb-2 text-center">About Me</h1>
       <div className="flex justify-center">
-        <h1 className="text-3xl font-bold mb-4 text-left max-w-2xl">
-        I&apos;m from Miami, FL and currently studying Industrial Engineering, MMSS, and CS at Northwestern University.<br />
-        I&apos;m passionate about product design, process optimization, technology, and AI.<br />
-        In my free time, I love painting, lifting and running, going to the beach, and watching thriller and horror movies.<br />
+        <h6 className="text-3xl font-bold mb-4 text-left max-w-2xl">
+        I&apos;m from Miami, FL and currently studying Industrial Engineering, MMSS, and CS at Northwestern University.<br /><br />
+        I&apos;m passionate about product design, process optimization, technology, and AI.<br /><br />
+        In my free time, I love painting, lifting and running, going to the beach, and watching thriller and horror movies.<br /><br />
         You can reach me at sebastianprusky2028@u.northwestern.edu.
-        </h1>
+        </h6>
       </div>
     </main>
   );

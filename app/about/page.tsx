0103f72784104b3be5b2243ba86e40d3e8f58f38
sebastianprusky <sebastianprusky2028@u.northwestern.edu>
2025-10-02 18:05:08 -0500

    fix erorroro

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,7 +7,7 @@ export default function About(): JSX.Element {
       <div className="flex justify-center">
   <p className="text-base font-normal mb-4 text-left max-w-2xl">
         <br />I&apos;m from Miami, FL and currently studying Industrial Engineering, Mathematical Methods in the Social Sciences, and CS at Northwestern University.<br /><br />
-        I&apos;m passionate about anything that combines math and tech to solve design, engineering, and business problems. I'm currently working on developing my technical skills through personal projects and getting hands-on business experience by working on projects with startups.<br /><br />
+  I&apos;m passionate about anything that combines math and tech to solve design, engineering, and business problems. I&apos;m currently working on developing my technical skills through personal projects and getting hands-on business experience by working on projects with startups.<br /><br />
         In my free time, I love painting, lifting and running, spending time at the beach, and watching thriller movies.<br /><br />
         You can reach me at sebastianprusky2028@u.northwestern.edu.
         </p>

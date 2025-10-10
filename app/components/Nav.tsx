4b00abf524f39a5d81a4a97e30ee891d22b8e5ac
sebastianprusky <sebastianprusky2028@u.northwestern.edu>
2025-10-10 13:30:03 -0500

    error

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export default function Nav(): JSX.Element {
+export default function Nav() {
   const pathname = usePathname() ?? "/";
 
   const links = [

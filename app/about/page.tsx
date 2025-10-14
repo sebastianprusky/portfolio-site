f04d691eb02b605f9fad248c26467b5944d3f55c
sebastianprusky <sebastianprusky2028@u.northwestern.edu>
2025-10-13 23:05:14 -0500

    animation, email

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -14,7 +14,7 @@ export default function About() {
         <br></br>I&apos;m from Miami, FL and currently studying Industrial Engineering, Mathematical Methods in the Social Sciences, and CS at Northwestern University.<br /><br />
         I&apos;m passionate about anything that combines math and tech to solve design, engineering, and business problems. I&apos;m currently working on developing my technical skills through personal projects and getting hands-on business experience by working on projects with startups.<br /><br />
         In my free time, I love painting, lifting and running, spending time at the beach, and watching thriller movies.<br /><br />
-        You can reach me at sebastianprusky2028@u.northwestern.edu.
+        You can reach me at sebprusky@u.northwestern.edu.
       </p>
     </main>
   );

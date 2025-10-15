cea0708c164095633bd9b57cccefcc47cc079c2a
sebastianprusky <sebastianprusky2028@u.northwestern.edu>
2025-10-14 20:42:07 -0500

    about text

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,7 +12,7 @@ export default function About() {
 
       <p className="text-base font-normal mb-4 text-left max-w-2xl">
         <br></br>I&apos;m from Miami, FL and currently studying Industrial Engineering, Mathematical Methods in the Social Sciences, and Computer Science at Northwestern University.<br /><br />
-        I&apos;m passionate about anything that combines math and tech to solve design, engineering, and business problems. I&apos;m currently working on developing my technical skills through personal projects and getting hands-on business experience by working on projects with startups.<br /><br />
+        I&apos;m passionate about combining math and tech to solve design, engineering, and business problems. I&apos;m currently working on developing my technical skills through personal projects and getting hands-on business experience by working on projects with startups.<br /><br />
         In my free time, I love painting, lifting and running, spending time at the beach, and watching thriller movies.<br /><br />
         You can reach me at sebastianprusky2028@u.northwestern.edu.
       </p>
